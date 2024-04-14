6e548c6857ca71e489e8c35d55ffde313ebd74db
chill <wiktor.ignasiak2@gmail.com>
2024-04-14 15:10:25 +0200

    bugfixes overall

--- js/navigation.js
+++ js/navigation.js
@@ -4,12 +4,12 @@
 
 const navMobile = document.querySelector(".nav-mobile");
 const navMobileBtn = document.querySelector(".hamburger");
-const navMobileMenu = document.querySelector(".nav-mobile__menu");
-const navMobileMenuItems = document.querySelectorAll(".nav-mobile__menu-item");
+const navMobileMenu = document.querySelector(".nav-mobile__link-box");
+const navMobileMenuItems = document.querySelectorAll(".nav-mobile__link-item");
 
 navMobileBtn.addEventListener("click", () => {
 	navMobile.classList.toggle("nav-mobile--active");
-	navMobileMenu.classList.toggle("nav-mobile__menu--active");
+	navMobileMenu.classList.toggle("nav-mobile__link-box--active");
 
 	if (document.body.classList.contains("disable-scroll")) {
 		document.body.classList.remove("disable-scroll");
@@ -21,7 +21,7 @@ navMobileBtn.addEventListener("click", () => {
 navMobileMenuItems.forEach((item) => {
 	item.addEventListener("click", () => {
 		navMobile.classList.remove("nav-mobile--active");
-		navMobileMenu.classList.remove("nav-mobile__menu--active");
+		navMobileMenu.classList.remove("nav-mobile__link-box--active");
 		document.body.classList.remove("disable-scroll");
 	});
 });
@@ -33,7 +33,7 @@ navMobileMenuItems.forEach((item) => {
 const navDesktop = document.querySelector(".nav-desktop");
 
 document.addEventListener("scroll", () => {
-	if (scrollY >= 200) {
+	if (scrollY >= 100) {
 		navDesktop.classList.add("nav-desktop--bg-active");
 	} else {
 		navDesktop.classList.remove("nav-desktop--bg-active");
--- js/testing.js
+++ js/testing.js
@@ -1,20 +1,21 @@
 const iconBoxAll = document.querySelectorAll(".icon-box");
-
-iconBoxAll.forEach((iconBox) => {
-	const svgElement = iconBox.querySelector("svg");
-
-	const iconBoxStyles = window.getComputedStyle(iconBox);
-	const iconBoxHeight = parseInt(iconBoxStyles.height);
-	const svgElementDimensions = iconBoxHeight * 0.8;
-
-	svgElement.setAttribute("height", svgElementDimensions);
-	svgElement.setAttribute("width", svgElementDimensions);
-});
-
 const iconBoxTextAll = document.querySelectorAll(".icon-box__text");
 
-iconBoxTextAll.forEach((iconBox) => {
-	const iconBoxSpan = iconBox.querySelector("span");
-	const iconBoxSpanTextContent = iconBoxSpan.textContent;
-	iconBoxSpan.innerHTML = `&nbsp;${iconBoxSpanTextContent}`;
+window.addEventListener("load", () => {
+	iconBoxAll.forEach((iconBox) => {
+		const svgElement = iconBox.querySelector("svg");
+
+		const iconBoxStyles = window.getComputedStyle(iconBox);
+		const iconBoxHeight = parseInt(iconBoxStyles.height);
+		const svgElementDimensions = iconBoxHeight * 0.8;
+
+		svgElement.setAttribute("height", svgElementDimensions);
+		svgElement.setAttribute("width", svgElementDimensions);
+	});
+
+	iconBoxTextAll.forEach((iconBox) => {
+		const iconBoxSpan = iconBox.querySelector("span");
+		const iconBoxSpanTextContent = iconBoxSpan.textContent;
+		iconBoxSpan.innerHTML = `&nbsp;${iconBoxSpanTextContent}`;
+	});
 });
